ae96c527ad88c61ca528106593efb4693c0d347d
Kazagumo <13860126292@163.com>
2022-10-26 22:43:04 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -16,3 +16,19 @@
 # Add a feed source
 #echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
+
+#添加unblockneteasemusic插件包
+git clone https://github.com/UnblockNeteaseMusic/luci-app-unblockneteasemusic.git package/x64_gxnas-Software
+
+#添加bypass插件包
+git clone https://github.com/kiddin9/openwrt-bypass.git package/x64_gxnas-Software
+
+#添加OpenClash插件包
+git clone https://github.com/vernesong/OpenClash.git package/x64_gxnas-Software
+
+#添加mosdns插件包
+git clone https://github.com/QiuSimons/openwrt-mos.git package/x64_gxnas-Software  
+
+#添加ssrplus+插件包
+git clone https://github.com/fw876/helloworld.git package/x64_gxnas-Software/ssrplus
+sed -i '12a entry({"admin", "vpn"}, firstchild(), "GFW", 45).dependent = false' package/x64_gxnas-Software/ssrplus/luci-app-ssr-plus/luasrc/controller/shadowsocksr.lua
